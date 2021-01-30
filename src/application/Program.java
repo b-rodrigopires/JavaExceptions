3de67441ce600c2dc010b2d9858cdcc9ae5a6488
Rodrigo Pires <b.rodrigopires@gmail.com>
2021-01-30 15:03:03 -0300

    ajustada identação do codigo

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,25 +14,25 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 		
 		try {
-		System.out.println("Enter account data");
-		System.out.print("Number: ");
-		int number = sc.nextInt();
-		System.out.print("Holder: ");
-		sc.nextLine();
-		String holder = sc.nextLine();
-		System.out.print("Initial balance: ");
-		double balance = sc.nextDouble();
-		System.out.print("Withdraw limit: ");
-		double withdrawLimit = sc.nextDouble();
-		
-		Account a = new Account(number, holder, balance, withdrawLimit);
-		
-		System.out.println();
-		System.out.print("Enter amount for withdraw: ");
-		double amount = sc.nextDouble();
-		a.withdraw(amount);
-		
-		System.out.println("new balance: " + String.format("%.2f", a.getBalance()));
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			System.out.print("Holder: ");
+			sc.nextLine();
+			String holder = sc.nextLine();
+			System.out.print("Initial balance: ");
+			double balance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			double withdrawLimit = sc.nextDouble();
+			
+			Account a = new Account(number, holder, balance, withdrawLimit);
+			
+			System.out.println();
+			System.out.print("Enter amount for withdraw: ");
+			double amount = sc.nextDouble();
+			a.withdraw(amount);
+			
+			System.out.println("new balance: " + String.format("%.2f", a.getBalance()));
 		}
 		catch(DomainException e) {
 			System.out.println("Withdraw error: " + e.getMessage());
